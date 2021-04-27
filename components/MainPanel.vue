a2dcf7a4c8798a0d40facc20fa7fe32fc3e7033b
hooman734 <hesamyan@outlook.com>
2021-04-27 20:05:07 +0400

    some progress

--- components/MainPanel.vue
+++ components/MainPanel.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex justify-center my-auto">
     <div class="flex-flex-col align-middle justify-center">
-<!--      Education     -->
+      <!--      Education     -->
       <div>
         <p class="font-bold text-gray-700 font-sans text-2xl">
           <font-awesome-icon icon="graduation-cap"/>
           Education
         </p>
-        <div class="ml-9">
+        <div class="ml-2 md:ml-9">
           <div class="flex flex-row justify-between">
             <p class="text-green-900 font-bold text-xl">
               <font-awesome-icon class="text-sm" icon="dot-circle"/>
@@ -56,80 +56,165 @@
           <progress-bar percent="50">Iran</progress-bar>
         </div>
       </div>
-<!--     Experience     -->
+      <!--     Experience     -->
       <div class="mt-3 md:mt-10">
         <p class="font-bold text-gray-700 font-sans text-2xl">
           <font-awesome-icon icon="briefcase"/>
           Experience
         </p>
-        <div class="ml-9">
+        <div class="ml-2 md:ml-9">
           <div class="flex flex-row justify-between">
             <p class="text-green-900 font-bold text-xl">
               <font-awesome-icon class="text-sm" icon="dot-circle"/>
-              American University of Armenia
+              Freelance
             </p>
-            <p class="text-green-900 font-bold">2017 - 2020</p>
+            <p class="text-green-900 font-bold">2021</p>
           </div>
           <div class="border-l-4 pl-5 pb-4">
-            <p class="text-gray-400 text-sm mb-1.5">Yerevan, Armenia</p>
+            <p class="text-gray-400 text-sm mb-1.5">Online</p>
             <p class="font-semibold">Master of Science in Computer and Information Science</p>
             <p class="font-semibold">Following are some prominent courses during my education at AUA</p>
+
+          </div>
+          <div class="flex flex-row justify-between">
+            <p class="text-green-900 font-bold text-xl">
+              <font-awesome-icon class="text-sm" icon="dot-circle"/>
+              AUA
+            </p>
+            <p class="text-green-900 font-bold">2020</p>
+          </div>
+          <div class="pl-5 pb-4">
+            <p class="text-gray-400 text-sm mb-1.5">Yerevan, Armenia</p>
+            <p class="font-semibold">Graduate School Internship Project </p>
             <ul class="list-inside list-disc">
-              <li>Data Structures</li>
-              <li>Algorithms (Intermediate and Advanced)</li>
-              <li>Object-Oriented Programming</li>
-              <li>Theory of Computing</li>
-              <li>Network Programming</li>
-              <li>Computer-Aided Manufacturing (CAM)</li>
-              <li>Cryptography</li>
-              <li>Image Processing</li>
+              <li>Used Python, TKinter to create a download-manager with chunking support and GUI S3 browser</li>
+              <li>Used Node.js, Socket.IO, and React/Redux to create an online chat room with channel support</li>
+              <li>Used .NET Core to create a public traffic transportation monitoring with route suggestions</li>
             </ul>
           </div>
-          <div class="flex flex-row justify-between">
-            <p class="text-green-900 font-bold text-xl">
-              <font-awesome-icon class="text-sm" icon="dot-circle"/>
-              Azad University
-            </p>
-            <p class="text-green-900 font-bold">2011 - 2014</p>
-          </div>
-          <div class="border-l-4 pl-5">
-            <p class="text-gray-400 text-sm mb-1.5">Gorgan, Iran</p>
-            <p class="font-semibold pb-4">Bachelor of Science in Civil Engineering</p>
-          </div>
-          <div class="flex flex-row justify-between">
-            <p class="text-green-900 font-bold text-xl">
-              <font-awesome-icon class="text-sm" icon="dot-circle"/>
-              Mazandaran University
-            </p>
-            <p class="text-green-900 font-bold">1999 - 2004</p>
-          </div>
-          <div class="pl-5">
-            <p class="text-gray-400 text-sm mb-1.5">Babol, Iran</p>
-            <p class="font-semibold">Bachelor of Science in Mechanical Engineering</p>
-          </div>
 
-
-      </div>
+        </div>
         <!--        Skills  -->
-        <div class="mt-3 md:mt-10">
+
+        <div class="mt-4">
           <p class="font-bold text-gray-700 font-sans text-2xl">
-            <font-awesome-icon icon="graduation-cap"/>
+            <font-awesome-icon icon="puzzle-piece"/>
             Technical Skills
           </p>
-          <div class="ml-9">
-            <div class="flex flex-row align-middle justify-start font-black">
-              <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">C#</p>
-              <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">C++</p>
-              <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Java</p>
-              <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Golang</p>
-              <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Python</p>
-              <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">JavaScript</p>
-              <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">TypeScript</p>
+          <div class="ml-2 md:ml-9">
+            <div class="flex flex-row justify-between">
+              <p class="text-green-900 font-bold text-xl">
+                <font-awesome-icon class="text-sm" icon="dot-circle"/>
+                Programming Language
+              </p>
             </div>
+            <div class="border-l-4 pl-5 pb-4">
+              <div class="flex flex-row align-middle justify-start font-black">
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">C#</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">C++</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Java</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Golang</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Python</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">JavaScript</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">TypeScript</p>
+              </div>
+            </div>
+            <div class="flex flex-row justify-between">
+              <p class="text-green-900 font-bold text-xl">
+                <font-awesome-icon class="text-sm" icon="dot-circle"/>
+                Front End
+              </p>
+            </div>
+            <div class="border-l-4 pl-5 pb-4">
+              <div class="flex flex-row align-middle justify-start font-black">
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">HTML</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">CSS</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">jQuery</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Tailwind</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Bootstrap</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Semantic-UI</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Angular</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">React</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Vue</p>
+              </div>
+            </div>
+            <div class="flex flex-row justify-between">
+              <p class="text-green-900 font-bold text-xl">
+                <font-awesome-icon class="text-sm" icon="dot-circle"/>
+                Back End
+              </p>
+            </div>
+            <div class="border-l-4 pl-5 pb-4">
+              <div class="flex flex-row align-middle justify-start font-black">
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Node.js</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Express.js</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">.NET Core</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Flask</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Django</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Spring</p>
+              </div>
+            </div>
+            <div class="flex flex-row justify-between">
+              <p class="text-green-900 font-bold text-xl">
+                <font-awesome-icon class="text-sm" icon="dot-circle"/>
+                Libraries
+              </p>
+            </div>
+            <div class="border-l-4 pl-5 pb-4">
+              <div class="flex flex-row align-middle justify-start font-black">
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Tkinter</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Sequelize.js</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Entity Framework.Core</p>
+              </div>
+            </div>
+            <div class="flex flex-row justify-between">
+              <p class="text-green-900 font-bold text-xl">
+                <font-awesome-icon class="text-sm" icon="dot-circle"/>
+                Database / Storage
+              </p>
+            </div>
+            <div class="border-l-4 pl-5 pb-4">
+              <div class="flex flex-row align-middle justify-start font-black">
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">SQLite</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">​MySQL</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Postgres</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Oracle SQL</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">MongoDB</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">S3</p>
+              </div>
+            </div>
+            <div class="flex flex-row justify-between">
+              <p class="text-green-900 font-bold text-xl">
+                <font-awesome-icon class="text-sm" icon="dot-circle"/>
+                Application Software
+              </p>
+            </div>
+            <div class="pl-5 pb-4">
+              <div class="flex flex-row align-middle justify-start font-black">
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Git</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Linux</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Heroku</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Vercel</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">Docker</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">docker-compose</p>
+                <p class="ring-gray-100 rounded bg-gray-100 bg-clip-padding p-1 m-1">localstack</p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <!--        -->
+        <div class="mt-3 md:mt-10">
+          <p class="font-bold text-gray-700 font-sans text-2xl">
+            <font-awesome-icon icon="wrench"/>
+            General Skills
+          </p>
+          <div class="ml-2 md:ml-9">
+            <progress-bar percent="70">C#</progress-bar>
+            <progress-bar percent="80">C++</progress-bar>
           </div>
 
-          <progress-bar percent="70">C#</progress-bar>
-          <progress-bar percent="80">C++</progress-bar>
+
 
         </div>
 
@@ -141,11 +226,11 @@
 
 <script>
 import Vue from 'vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faGraduationCap, faDotCircle, faBriefcase } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faGraduationCap, faDotCircle, faBriefcase, faPuzzlePiece, faWrench} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
-library.add(faGraduationCap, faDotCircle, faBriefcase)
+library.add(faGraduationCap, faDotCircle, faBriefcase, faPuzzlePiece, faWrench)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="container mx-auto my-auto">
+  <div class="container mx-auto my-auto flex-wrap">
     <div class="flex flex-row flex-wrap justify-items-center mt-6 md:mt-20">
-      <div class="flex-initial mx-4 border-4 lg:w-1/5">
+      <div class="flex-initial mx-4 lg:w-1/5">
         <photo></photo>
       </div>
-      <div class="flex flex-1 mx-4 border-4 justify-center md:w-4/5">
+      <div class="flex flex-1 mx-4 justify-center md:w-4/5">
         <name class="self-center"></name>
       </div>
     </div>
 <!--    -->
     <div class="flex flex-row flex-wrap justify-items-center mt-6 md:mt-10">
-      <div class="flex-initial mx-4 border-4 md:w-2/6">
+      <div class="flex-initial mx-4 md:mx-10 lg:w-1/6">
         <side-panel></side-panel>
       </div>
-      <div class="flex-1 mx-4 border-4 md:w-4/6">
+      <div class="flex-1 mx-4 md:w-5/6">
         <main-panel></main-panel>
       </div>
     </div>
