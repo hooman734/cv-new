11fc2362f2b5c23808988ff4f7168e6ea2d5b478
hesamyan734 <hesamyan@outlook.com>
2022-08-22 00:18:41 +0400

    added the custom component named Experience

--- components/Experience.vue
+++ components/Experience.vue
@@ -1,21 +1,21 @@
 <template>
-    <div ref="body2" class="ml-2 md:ml-9">
-        <div class="flex flex-row flex-wrap justify-between">
-        <p class="text-green-900 font-bold">
-            <font-awesome-icon class="text-xs" icon="dot-circle"/>
-            {{topic}}
-        </p>
-        <p class="text-green-900 font-bold">{{date}}</p>
-        </div>
-        <div class="border-l-4 pl-5 pb-4 text-justify break-all">
-        <p class="text-gray-400 text-sm mb-1.5">{{subTopic}}</p>
-        <p class="">{{description1}}</p>
-        <p class="">{{description2}}</p>
-        <ul class="list-inside list-disc">
-            <li v-for="(item, idx) in items" :key="idx">{{item}}</li>
-        </ul>
-        </div>
+  <div ref="body2" class="ml-2 md:ml-9">
+    <div class="flex flex-row flex-wrap justify-between">
+      <p class="text-green-900 font-bold">
+        <font-awesome-icon class="text-xs" icon="dot-circle" />
+        {{ topic }}
+      </p>
+      <p class="text-green-900 font-bold">{{ date }}</p>
     </div>
+    <div class="border-l-4 pl-5 pb-4 text-justify break-all">
+      <p class="text-gray-400 text-sm mb-1.5">{{ subTopic }}</p>
+      <p class="font-bold">{{ description1 }}</p>
+      <p class="">{{ description2 }}</p>
+      <ul class="list-inside list-disc">
+        <li v-for="(item, idx) in items" :key="idx">{{ item }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
